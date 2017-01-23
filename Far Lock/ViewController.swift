009b05c77dc0cc58417c692006b897916fae58da
Karthik <karthik.ms@zohocorp.com>
2017-01-23 15:28:24 +0530

    Initial Commit

---
+++ Far Lock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Far Lock
+//
+//  Created by Karthik M S on 23/01/17.
+//  Copyright © 2017 Karthik M S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
