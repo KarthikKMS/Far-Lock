ef0f179b03a3e7f91b9fdf93e06265be007cfc8a
Karthik <karthik.ms@zohocorp.com>
2017-01-23 20:03:40 +0530

    Added BluetoothOFFViewController

---
+++ Far Lock/Controller/DummyViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DummyViewController.swift
+//  Far Lock
+//
+//  Created by Karthik M S on 23/01/17.
+//  Copyright © 2017 Karthik M S. All rights reserved.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func bluetoothOffButtonPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "BluetoothOffInDummyViewController", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
