052d3f4b99a2fc48a76f4420053f4bad5d61ff5e
Karthik <karthik.ms@zohocorp.com>
2017-01-24 00:09:09 +0530

    Added EnterPasswordViewController

---
+++ Far Lock/Controller/EnterPasswordViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EnterPasswordViewController.swift
+//  Far Lock
+//
+//  Created by Karthik M S on 23/01/17.
+//  Copyright © 2017 Karthik M S. All rights reserved.
+//
+
+import UIKit
+
+class EnterPasswordViewController: UIViewController {
+
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var connectButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        usernameTextField.layer.borderColor = UIColor.white.cgColor
+        passwordTextField.layer.borderColor = UIColor.white.cgColor
+        connectButton.layer.borderColor = UIColor.white.cgColor
+        cancelButton.layer.borderColor = UIColor.white.cgColor
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
