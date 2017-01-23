d4b8463d0ab7ba3898315b70a78f7066297b3e44
Karthik <karthik.ms@zohocorp.com>
2017-01-24 00:25:37 +0530

    Added wrongPasswordView in EnterPasswordViewController

--- Far Lock/Controller/EnterPasswordViewController.swift
+++ Far Lock/Controller/EnterPasswordViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class EnterPasswordViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var rootStackView: UIStackView!
     @IBOutlet weak var deviceNameLabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    
+    @IBOutlet weak var wrongPasswordView: UIView!
+    @IBOutlet weak var tryAgainButton: UIButton!
+    
+    
+    // MARK: - ViewController Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +32,29 @@ class EnterPasswordViewController: UIViewController {
         passwordTextField.layer.borderColor = UIColor.white.cgColor
         connectButton.layer.borderColor = UIColor.white.cgColor
         cancelButton.layer.borderColor = UIColor.white.cgColor
+        tryAgainButton.layer.borderColor = UIColor.white.cgColor
     }
     
+    
+    // MARK: - IBActions
+    
+    @IBAction func connectButtonPressed(_ sender: UIButton) {
+        // delete 
+        rootStackView.alpha = 0.1
+        wrongPasswordView.isHidden = false
+        // delete
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func tryAgainButtonPressed(_ sender: UIButton) {
+        rootStackView.alpha = 1
+        wrongPasswordView.isHidden = true
+    }
 
+    
     /*
     // MARK: - Navigation
 
