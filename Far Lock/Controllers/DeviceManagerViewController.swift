2dbc1a8f68baddc9bee42b83d84b3dd8ea1d4726
Karthik <karthik.ms@zohocorp.com>
2017-01-25 00:31:25 +0530

    Added DeviceManagerViewController

---
+++ Far Lock/Controllers/DeviceManagerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DeviceManagerViewController.swift
+//  Far Lock
+//
+//  Created by Karthik M S on 24/01/17.
+//  Copyright © 2017 Karthik M S. All rights reserved.
+//
+
+import UIKit
+
+class DeviceManagerViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    
+    // MARK: - ViewController Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    // MARK: - IBActions
+
+    @IBAction func helpButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func addDeviceButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+}
--- Far Lock/Controllers/EnterPasswordViewController.swift
+++ Far Lock/Controllers/EnterPasswordViewController.swift
@@ -61,15 +61,5 @@ class EnterPasswordViewController: UIViewController {
     @IBAction func continueButtonPressed(_ sender: UIButton) {
         
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
